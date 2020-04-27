e3b6fb301a485e38b4db4e6275b4a260d5b8a956
Wael <wael.natafji0003@stud.hkr.se>
2020-04-27 15:43:12 +0200

    Bugs

--- src/sample/databaseConnection/Staff.java
+++ src/sample/databaseConnection/Staff.java
@@ -2,35 +2,34 @@ package sample.databaseConnection;
 
 import java.sql.*;
 
-public class Staff extends Connect{
+public class Staff extends Connect {
 
     private Connection connection;
     private Statement statement;
     private ResultSet resultSet;
 
-    public Staff(){
+    public Staff() {
     }
 
 
-    public void insertIntoPStaffTable(){
+    public void insertIntoPStaffTable() {
     }
 
-    public void updateIntoStaffTable(){
+    public void updateIntoStaffTable() {
 
     }
-    public void viewStaffTable(){
+    public void viewStaffTable() {
 
     }
     public int verifyStaffLogin(String username, String password) { // The Log in verification method. Returns 1 incase found, else
         try {
             connection = DriverManager.getConnection(Connect.CONNECTION_URL, Connect.DB_NAME, Connect.PASSWORD);
             statement = connection.createStatement();
-            resultSet = statement.executeQuery("Select `Username`, `Password` from `Log in` where Username = "+username+" AND "+password+";  ");
+            resultSet = statement.executeQuery("Select `Username`, `Password` from `Log in` where Username = " + username + " AND " + password + ";  ");
 
-        }catch (SQLException a) {
+        } catch (SQLException a) {
 
         }
-return 0;
+        return 0;
     }
-
 }

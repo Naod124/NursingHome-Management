28f8cd817657baa119bef6587b75c774a3cf5962
Wael <wael.natafji0003@stud.hkr.se>
2020-04-29 14:31:31 +0200

    addNewEmployee

--- src/sample/controller/AddNewEmployeeController.java
+++ src/sample/controller/AddNewEmployeeController.java
@@ -4,6 +4,7 @@ import com.jfoenix.controls.JFXComboBox;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import sample.model.*;
@@ -15,6 +16,8 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class AddNewEmployeeController implements Initializable {
+    @FXML
+    private ChoiceBox<String> chooseRole;
     @FXML
     private JFXComboBox<String> employeeRole;
     @FXML
@@ -41,26 +44,43 @@ public class AddNewEmployeeController implements Initializable {
     private static ArrayList<Staff> staff = new ArrayList<>();
 
     public void add(ActionEvent actionEvent) throws FileNotFoundException {
+        String firstNameO = firstName.getText();
+        String lastNameO    = lastName.getText();
+        String addressO = address.getText();
+        String SSN = ssn.getText();
+        int ageO = Integer.parseInt(age.getText());
+        String Email = email.getText();
+        String username = userName.getText();
+        String password = passWord.getText();
+        String role = chooseRole.getValue();
+        switch (role){
+            case "Nurse":
+                LogIn logIn = new LogIn(username,password);
+                Nurse nurse = new Nurse(firstNameO,lastNameO,addressO,SSN,ageO,Email,logIn,20000);
+            case "Planer":
+                LogIn logIn1 = new LogIn(username,password);
+                Nurse planer = new Nurse(firstNameO,lastNameO,addressO,SSN,ageO,Email,logIn1,20000);
+            case "Assistant":
+                LogIn logIn2 = new LogIn(username,password);
+                Nurse assistant = new Nurse(firstNameO,lastNameO,addressO,SSN,ageO,Email,logIn2,20000);
+        }
 
     }
 
     public void cancel(ActionEvent actionEvent) throws IOException {
-        sc.newScene(actionEvent , "view/admin.fxml");
+        sc.newScene(actionEvent , "/sample/view/admin.fxml");
     }
 
     public void help(ActionEvent actionEvent) {
     }
 
     public void backToMain(ActionEvent actionEvent) throws IOException {
-        sc.newScene(actionEvent , "view/logIn.fxml");
+        sc.newScene(actionEvent , "/sample/view/logIn.fxml");
     }
 
-    public void chooseRole(ActionEvent actionEvent) {
-
-    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        chooseRole.getItems().addAll("Nurse","Planer","Assistant");
     }
 }

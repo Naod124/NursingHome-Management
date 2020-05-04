5aa3a1b4982a54350b663b82e5b706d20eb5256f
Wael <wael.natafji0003@stud.hkr.se>
2020-05-04 15:47:29 +0200

    planer class

---
+++ src/sample/model/Planer.java
@@ -0,0 +1,7 @@
+package sample.model;
+
+public class Planer extends Staff {
+    public Planer(String firstName, String lastName, String address, String SSN, String dOb, String eMail, LogIn logIn, double salary) {
+        super(firstName, lastName, address, SSN, dOb, eMail, logIn, salary);
+    }
+}

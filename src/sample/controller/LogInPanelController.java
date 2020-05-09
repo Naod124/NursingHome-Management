28d1fa842901af16891ee191d086a3bb09271c45
Wael <wael.natafji0003@stud.hkr.se>
2020-05-09 17:29:54 +0200

    show password/hide password implemented/forgot password

--- src/sample/controller/LogInPanelController.java
+++ src/sample/controller/LogInPanelController.java
@@ -2,19 +2,28 @@ package sample.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import sample.databaseConnection.StaffQueries;
 
 import java.io.IOException;
+import java.net.URL;
 import java.sql.*;
+import java.util.ResourceBundle;
 
 public class LogInPanelController {
-    @FXML private TextField passWord;
-    @FXML private TextField userName;
+    @FXML
+    private CheckBox showPass;
+    @FXML
+    private PasswordField passWord;
+    @FXML
+    private TextField userName;
     private SwitchScene sc = new SwitchScene();
 
     public void back(ActionEvent actionEvent) throws IOException {
-      sc.newScene(actionEvent,"/sample/view/LogIn.fxml");
+        sc.newScene(actionEvent, "/sample/view/LogIn.fxml");
     }
 
     public void logIn(ActionEvent actionEvent) throws IOException, SQLException {//i will add later some sql statements once the database is done
@@ -23,19 +32,35 @@ public class LogInPanelController {
 
 
         if (row == 1 && userName.getText().endsWith("@admin.com")) {
-                SwitchScene sc = new SwitchScene();
-                sc.newScene(actionEvent, "/sample/view/admin.fxml");
+            SwitchScene sc = new SwitchScene();
+            sc.newScene(actionEvent, "/sample/view/admin.fxml");
 
-            }else if (row == 1 && userName.getText().endsWith("@nurse.com")){
+        } else if (row == 1 && userName.getText().endsWith("@nurse.com")) {
             SwitchScene sc = new SwitchScene();
             sc.newScene(actionEvent, "/sample/view/nurse.fxml");
 
-        }else if (row == 1 && userName.getText().endsWith("@planer.com")){
+        } else if (row == 1 && userName.getText().endsWith("@planer.com")) {
             SwitchScene sc = new SwitchScene();
             sc.newScene(actionEvent, "/sample/view/planer.fxml");
 
-        }else{
+        } else {
             System.out.println("Error");
         }
+    }
+
+    public void showPass(ActionEvent actionEvent) {
+        if (showPass.isSelected()) {
+            passWord.setPromptText(passWord.getText());
+            passWord.setText("");
+            passWord.setDisable(true);
+
+        }else {
+            passWord .setText(passWord.getPromptText());
+            passWord.setPromptText("");
+            passWord.setDisable(false);
+        }
+    }
+
+    public void forgetPassword(ActionEvent actionEvent) {
     }
 }
